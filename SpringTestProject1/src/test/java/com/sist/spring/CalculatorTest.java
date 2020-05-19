5feac49838510040f4fe620a9abfe5026dd617b6
caskercasker <caskercasker@daum.net>
2020-05-19 10:17:59 +0900

    pom.xml 버전 업

--- SpringTestProject1/src/test/java/com/sist/spring/CalculatorTest.java
+++ SpringTestProject1/src/test/java/com/sist/spring/CalculatorTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+/*@RunWith(SpringJUnit4ClassRunner.class)*/
 public class CalculatorTest {
 	@Test
 	public void testSum(){
